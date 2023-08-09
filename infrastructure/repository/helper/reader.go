f679ccae0e0ce7af5a01c8fcb5d9f2f12ecdae55
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2023-08-10 02:26:34 +0700

    fix list helper

--- infrastructure/repository/helper/reader.go
+++ infrastructure/repository/helper/reader.go
@@ -6,7 +6,7 @@ import (
 
 func (r *Repository) List(lat float64, lon float64, radius float64) ([]helper.Helper, error) {
 	var helpers []helper.Helper
-	result := r.db.Raw("SELECT * FROM helpers WHERE lat BETWEEN ? AND ? AND lon BETWEEN ? AND ?", lat+radius, lat-radius, lon+radius, lon-radius).Scan(&helpers)
+	result := r.db.Raw("SELECT * FROM helpers WHERE lat BETWEEN ? AND ? AND lon BETWEEN ? AND ?", lat-radius, lat+radius, lon-radius, lon+radius).Scan(&helpers)
 	if result.Error != nil {
 		return []helper.Helper{}, result.Error
 	}
