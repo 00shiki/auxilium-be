b176b42d39662d2008b2096805a06bce6d30a2d5
Ahmad Luhur Pakerti <ahmadluhurp@gmail.com>
2023-08-10 02:19:45 +0700

    fix remove helper

--- infrastructure/repository/helper/writer.go
+++ infrastructure/repository/helper/writer.go
@@ -11,7 +11,7 @@ func (r *Repository) Create(helper *helper.Helper) error {
 }
 
 func (r *Repository) Remove(userID uint) error {
-	result := r.db.Exec("DELETE helpers WHERE user_id = ?", userID)
+	result := r.db.Exec("DELETE FROM helpers WHERE user_id = ?", userID)
 	if result.Error != nil {
 		return result.Error
 	}
